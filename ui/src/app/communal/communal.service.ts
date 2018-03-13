5a0c9bf2c64d628368227ec606d3b5dc8482cf36
Lana-sloth <lana.sloth@gmail.com>
2018-03-13 12:30:09 +0300

    server added, information cards (not functionsl yet) moved into communal component

--- ui/src/app/communal/communal.service.ts
+++ ui/src/app/communal/communal.service.ts
@@ -3,38 +3,38 @@ import { Http } from '@angular/http';
 
 @Injectable()
 export class CommunalService {
-    constructor(private http: Http){}
+  constructor(private http: Http){}
 
-    getCommunals(){
-        return [
-            { 
-              title: 'Cold Water',
-              tax: 35.40,
-              last: 0,
-              current: 0,
-              spent: 0
-            },
-            { 
-              title: 'Hot Water',
-              tax: 180.55,
-              last: 0,
-              current: 0,
-              spent: 0
-            },
-            { 
-              title: 'Day Electricity',
-              tax: 6.19,
-              last: 0,
-              current: 0,
-              spent: 0
-            },
-            { 
-              title: 'Night Electricity',
-              tax: 1.79,
-              last: 0,
-              current: 0,
-              spent: 0
-            }
-          ]
-    }
+  getCommunals(){
+    return [
+      { 
+        title: 'Cold Water',
+        tax: 35.40,
+        last: 0,
+        current: 0,
+        spent: 0
+      },
+      { 
+        title: 'Hot Water',
+        tax: 180.55,
+        last: 0,
+        current: 0,
+        spent: 0
+      },
+      { 
+        title: 'Day Electricity',
+        tax: 6.19,
+        last: 0,
+        current: 0,
+        spent: 0
+      },
+      { 
+        title: 'Night Electricity',
+        tax: 1.79,
+        last: 0,
+        current: 0,
+        spent: 0
+      }
+    ]
+  }
 }
--- ui/src/app/communal/containers/communal/communal.component.ts
+++ ui/src/app/communal/containers/communal/communal.component.ts
@@ -12,12 +12,243 @@ import { CommunalService } from '../../communal.service';
   template: `
     <br>
     <div class='row'>
-      <card-component class='col-md-3'
+      <!-- <card-component class='col-md-3'
         *ngFor='let communal of communals'
         [card]='communal'>
-      </card-component>
+      </card-component> -->
+      
+      <!-- ///////////////////// -->
+      <!--      COLD WATER       -->
+      <!-- ///////////////////// -->
+      <div class='col-md-3'>
+        <div class='card' >
+          <div class='card-body'>
+  
+            <!-- title -->
+            <h5 class='card-title'>Water</h5>
+            <!-- tax link -->
+            <p>Tax: 
+            <span 
+            class='taxLink'>
+                Tax &#8381;
+            </span>
+            </p>
+            <!-- tax input group -->
+            <div class='input-group  mb-3'>
+              <div class='input-group-prepend'>
+                <span class='input-group-text communal-label'>tax</span>
+              </div>
+              <input type='number' class='form-control'>
+            </div> <!-- end of  tax input group  -->
+
+            <!-- last month input group -->
+            <div class="input-group  mb-3">
+              <div class="input-group-prepend">
+                <span class="input-group-text communal-label">last</span>
+              </div>
+              <input type='number' class="form-control">
+            </div> <!-- end of last month input group -->
+            
+            <!-- this month input group -->
+            <div class='input-group  mb-3'>
+              <div class='input-group-prepend'>
+                <span class='input-group-text communal-label'>now</span>
+              </div>
+              <input type='number' class='form-control'>
+            </div> <!-- end of this month input group -->
+
+            <hr>
+            <table>
+                <tr>
+                  <th>Delta:</th>
+                  <td>...</td>
+                </tr>
+                <tr>
+                  <th>Spent:</th>
+                  <td>... &#8381;</td>
+                </tr>
+            </table>
+  
+          </div> <!-- end of  card body  -->
+        </div> <!-- end of  card  -->
+      </div> <!-- end of  column  -->
+      
+          
+      <!-- ///////////////////// -->
+      <!--      HOT WATER        -->
+      <!-- ///////////////////// -->
+      <div class='col-md-3'>
+        <div class='card' >
+          <div class='card-body'>
+  
+            <!-- title -->
+            <h5 class='card-title'>Water</h5>
+            <!-- tax link -->
+            <p>Tax: 
+              <span 
+              class='taxLink'>
+                Tax &#8381;
+              </span>
+            </p>
+            <!-- tax input group -->
+            <div class='input-group  mb-3'>
+              <div class='input-group-prepend'>
+                <span class='input-group-text communal-label'>tax</span>
+              </div>
+              <input type='number' class='form-control'>
+            </div> <!-- end of  tax input group  -->
+  
+            <!-- last month input group -->
+            <div class="input-group  mb-3">
+              <div class="input-group-prepend">
+                <span class="input-group-text communal-label">last</span>
+              </div>
+              <input type='number' class="form-control">
+            </div> <!-- end of last month input group -->
+            
+            <!-- this month input group -->
+            <div class='input-group  mb-3'>
+              <div class='input-group-prepend'>
+                <span class='input-group-text communal-label'>now</span>
+              </div>
+              <input type='number' class='form-control'>
+            </div> <!-- end of this month input group -->
+  
+            <hr>
+            <table>
+              <tr>
+                <th>Delta:</th>
+                <td>...</td>
+              </tr>
+              <tr>
+                <th>Spent:</th>
+                <td>... &#8381;</td>
+              </tr>
+            </table>
+  
+          </div> <!-- end of  card body  -->
+        </div> <!-- end of  card  -->
+      </div> <!-- end of  column  -->
+
+
+      <!-- ///////////////////// -->
+      <!--    DAY ELECTIRCITY    -->
+      <!-- ///////////////////// -->
+      <div class='col-md-3'>
+        <div class='card' >
+          <div class='card-body'>
+  
+            <!-- title -->
+            <h5 class='card-title'>Water</h5>
+            <!-- tax link -->
+            <p>Tax: 
+              <span 
+              class='taxLink'>
+                Tax &#8381;
+              </span>
+            </p>
+            <!-- tax input group -->
+            <div class='input-group  mb-3'>
+              <div class='input-group-prepend'>
+                <span class='input-group-text communal-label'>tax</span>
+              </div>
+              <input type='number' class='form-control'>
+            </div> <!-- end of  tax input group  -->
+  
+            <!-- last month input group -->
+            <div class="input-group  mb-3">
+              <div class="input-group-prepend">
+                <span class="input-group-text communal-label">last</span>
+              </div>
+              <input type='number' class="form-control">
+            </div> <!-- end of last month input group -->
+            
+            <!-- this month input group -->
+            <div class='input-group  mb-3'>
+              <div class='input-group-prepend'>
+                <span class='input-group-text communal-label'>now</span>
+              </div>
+              <input type='number' class='form-control'>
+            </div> <!-- end of this month input group -->
+  
+            <hr>
+            <table>
+              <tr>
+                <th>Delta:</th>
+                <td>...</td>
+              </tr>
+              <tr>
+                <th>Spent:</th>
+                <td>... &#8381;</td>
+              </tr>
+            </table>
+  
+          </div> <!-- end of  card body  -->
+        </div> <!-- end of  card  -->
+      </div> <!-- end of  column  -->
+
+
+      <!-- ///////////////////// -->
+      <!--   NIGHT ELECTIRCITY   -->
+      <!-- ///////////////////// -->
+      <div class='col-md-3'>
+        <div class='card' >
+          <div class='card-body'>
+  
+            <!-- title -->
+            <h5 class='card-title'>Water</h5>
+            <!-- tax link -->
+            <p>Tax: 
+              <span 
+              class='taxLink'>
+                Tax &#8381;
+              </span>
+            </p>
+            <!-- tax input group -->
+            <div class='input-group  mb-3'>
+              <div class='input-group-prepend'>
+                <span class='input-group-text communal-label'>tax</span>
+              </div>
+              <input type='number' class='form-control'>
+            </div> <!-- end of  tax input group  -->
+  
+            <!-- last month input group -->
+            <div class="input-group  mb-3">
+              <div class="input-group-prepend">
+                <span class="input-group-text communal-label">last</span>
+              </div>
+              <input type='number' class="form-control">
+            </div> <!-- end of last month input group -->
+            
+            <!-- this month input group -->
+            <div class='input-group  mb-3'>
+              <div class='input-group-prepend'>
+                <span class='input-group-text communal-label'>now</span>
+              </div>
+              <input type='number' class='form-control'>
+            </div> <!-- end of this month input group -->
+  
+            <hr>
+            <table>
+              <tr>
+                <th>Delta:</th>
+                <td>...</td>
+              </tr>
+              <tr>
+                <th>Spent:</th>
+                <td>... &#8381;</td>
+              </tr>
+            </table>
+  
+          </div> <!-- end of  card body  -->
+        </div> <!-- end of  card  -->
+      </div> <!-- end of  column  -->
+
     </div> <!-- end of row -->
 
+    <!-- ///////////////////// -->
+    <!--         TOTAL         -->
+    <!-- ///////////////////// -->
     <br>
     <div class="alert alert-primary">
       <h3 class="text-center"> Total: {{ countTotal() }} &#8381; </h3>
